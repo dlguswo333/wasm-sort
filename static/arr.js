1e09ef4881627714d1848f72da561378329b2dba
dlguswo333 <dlguswo002@gmail.com>
2022-06-01 17:32:51 +0900

    Improve createArray function

--- static/arr.js
+++ static/arr.js
@@ -2,10 +2,10 @@
  * @param {number} len
  */
 export const createArray = (len) => {
-  const arr = [];
+  const arr = new Array(len).fill(0);
   for(let i=0;i<len;++i){
-    const tmp = Math.random()*10000000 | 0;
-    arr.push(tmp);
+    const tmp = Math.floor(Math.random()*100000000);
+    arr[i] = tmp;
   }
   return arr;
 }
